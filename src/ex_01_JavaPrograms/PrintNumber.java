84d37ccb2b9c98ba91c6acbc11934699ca96459f
ASHIF RAZA <ashifraza7870@gmail.com>
2025-09-30 16:20:35 +0530

    Print Number

---
+++ src/ex_01_JavaPrograms/PrintNumber.java
@@ -0,0 +1,9 @@
+package ex_01_JavaPrograms;
+
+public class PrintNumber {
+    public static void main (String[] args){
+        for(int i=0; i<=8; i++){
+            System.out.println(i);
+        }
+    }
+}
