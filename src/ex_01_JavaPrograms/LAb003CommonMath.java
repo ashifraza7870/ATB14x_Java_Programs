9d2d925d4dbf91f5e1e852560689b529d6e3ee84
ASHIF RAZA <ashifraza7870@gmail.com>
2025-10-01 14:12:38 +0530

    CommonMath

---
+++ src/ex_01_JavaPrograms/LAb003CommonMath.java
@@ -0,0 +1,11 @@
+package ex_01_JavaPrograms;
+
+public class LAb003CommonMath {
+    public static void main (String[] args){
+        System.out.println(2+2);
+        System.out.println(3*2);
+        System.out.println(5-2);
+        System.out.println(3/2); // The result should be 1.5 but println print only integer value
+        System.out.println("2+2");
+    }
+}
