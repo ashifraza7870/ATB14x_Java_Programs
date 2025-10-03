e193df92f5a4046477e942b7cd5621d4601db552
ASHIF RAZA <ashifraza7870@gmail.com>
2025-10-04 00:06:47 +0530

    Concatination

---
+++ src/ex_04Operators/Lab011_Concatination.java
@@ -0,0 +1,10 @@
+package ex_04Operators;
+
+public class Lab011_Concatination {
+    public static void main (String[] args){
+
+        String first_name = "Ashif";
+        String last_name = "Hashmi";
+        System.out.println(first_name+last_name);   // + operator behave differently when it's use with string
+    }
+}
