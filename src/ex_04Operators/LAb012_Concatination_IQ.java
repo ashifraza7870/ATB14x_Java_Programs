c05ad95e6d406a7176ca69d84494d66a9f20989f
ASHIF RAZA <ashifraza7870@gmail.com>
2025-10-04 15:19:38 +0530

    Concatination_IQ

---
+++ src/ex_04Operators/LAb012_Concatination_IQ.java
@@ -0,0 +1,12 @@
+package ex_04Operators;
+
+public class LAb012_Concatination_IQ {
+    public static void main (String[] args){
+        String firs_name ="Ashif";
+        String last_name ="Raza";
+
+        int a=10;
+        int b=15;
+        System.out.println(firs_name+last_name +a+b);
+    }
+}
