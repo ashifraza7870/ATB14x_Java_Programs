cdcc0071dde0f888bcd78985b6c4db66c313b1d1
ASHIF RAZA <ashifraza7870@gmail.com>
2025-10-13 18:39:48 +0530

    IfElse

---
+++ src/ex_08_IF_Cond/Lab025_If.java
@@ -0,0 +1,20 @@
+package ex_08_IF_Cond;
+
+import java.util.Scanner;
+
+public class Lab025_If {
+    public static void main (String[] args){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enetr the number");
+        int age  = s.nextInt();
+
+        if (age>18){
+            System.out.println("Yes you can vote");
+        }
+        else{
+            System.out.println("You can't vote");
+        }
+    }
+}
+//.....................................Chapter Efelse 50 sec.....................................
